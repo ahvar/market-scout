fafc0da4c14d6d58e17aa22ca461aa047b389562
Arthur Vargas <ahvargas92@gmail.com>
2024-02-25 12:08:05 -0600

    one more unit test passing: 64% cov

--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import MagicMock, patch
 from typer.testing import CliRunner
 from src.app import app
 from datetime import datetime
@@ -15,24 +16,40 @@ class TestCLI(unittest.TestCase):
         """
         self.runner = CliRunner()
 
-    def test_historical_quote_valid(self):
+    @patch("src.app.logger")
+    @patch("src.app.IBApiClient")
+    @patch("src.app.IBMarketMemory")
+    @patch("src.app.ContractFactory")
+    @patch("src.app.time.sleep")
+    def test_historical_quote_valid(
+        self, mock_sleep, mock_contract_factory, mock_memory, mock_client, mock_logger
+    ):
         """
         Test the 'quote' command.
+        :param mock_sleep: mock time.sleep
+        :param mock_contract_factory: mock ContractFactory
+        :param mock_memory: mock IBMarketMemory
+        :param mock_client: mock IBApiClient
+        :param mock_logger: mock logger
         """
-        # Assuming the quote command returns or prints something, you can check the outcome here.
         result = self.runner.invoke(
             app,
             [
                 "historical-quote",
                 "appl",
-                "-b",
+                "-br",
                 "1 hour",
-                "-d",
+                "-dr",
                 "1 D",
                 "-ed",
-                "3 D",
+                "2023-12-12",
                 "-et",
                 "12:00:00",
+                "-o",
+                "appl.csv",
             ],
         )
+        mock_contract_factory.assert_called()
+        mock_memory.assert_called()
+        mock_client.assert_called()
         self.assertEqual(result.exit_code, 0)
