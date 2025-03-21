76f0482160e50a86a6f7d253a33a006811c8aa24
Arthur Vargas <arthurvargas@Mac.attlocal.net>
2025-03-21 12:36:01 -0500

    leftover changes

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,34 +1,4 @@
 """
---------------------------
-Defining a starting point:
--------------------------- 
-The __name__ variable is a Python predefined variable, which is set to the name of the module
-in which it is used. It is a starting point for Flask to know where to look for templates, static
-files, and so on. 
-
------------------------------------------
-The "app" package vs. the "app" variable:
------------------------------------------
-The app package is defined by the app directory and the __init__.py file, and
-is used to import the app variable. The app variable is an instance of the Flask class.
-
-------------------
-The bottom import:
-------------------
-The routes module is imported at the bottom and not at the top of the script as it is always done.
-The bottom import is a workaround to circular imports, a common problem in Flask applications.
-
---------------
-Configurations:
---------------
-The pattern used for importing configurations is similar to the Flask class. The module is lowercase
-and the imported class uppercase. 
-
------------------------------
-Database and migration engine:
------------------------------
-There is a database object and a database migration engine instance. Alembic is the migration
-framework used by Flask-Migrate
 """
 
 from flask import Flask
@@ -45,4 +15,4 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
 from src.app import routes
-from src.app.models import user, trade, profit_and_loss
+from src.app.models import researcher, trade, profit_and_loss
